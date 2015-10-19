4297e8389a4f820a87661b0eda919ff92f58dedc
fawcbe01 <fawcbe01@luther.edu>
2015-10-18 22:00:18 -0500

    Update finalproject.js

--- finalproject.js
+++ finalproject.js
@@ -1,6 +1,6 @@
 newtask = function() {
   document.createElement("li");
   document.createElement("checkbox");
+  newtask = document.querySelector('#entertask').value;
   document.createTextNode("newtask");
-  newtask = document.querySelector("#entertask").value;
 }
