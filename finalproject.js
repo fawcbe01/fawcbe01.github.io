bdd78b7c795a68d641c3d9c7c0096239c0a29742
fawcbe01 <fawcbe01@luther.edu>
2015-10-18 22:32:44 -0500

    Update finalproject.js

--- finalproject.js
+++ finalproject.js
@@ -1,6 +1,6 @@
 newTask = function() {
   document.createElement('li');
-  document.body.appendChild('li');
+  document.body.appendChild('ul');
   document.createElement('checkbox');
   taskenter = document.querySelector('#entertext').value;
   document.createTextNode('taskenter');
