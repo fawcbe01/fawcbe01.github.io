014ffd34db00f76a9118c6e463a4d499260db0bb
fawcbe01 <fawcbe01@luther.edu>
2015-10-19 14:00:48 -0500

    Update finalproject.js

--- finalproject.js
+++ finalproject.js
@@ -1,6 +1,7 @@
 newTask = function() {
   myUl = document.querySelector('#tasklist')
   newli = document.createElement('li');
+  myUl.appendChild("newli");
   newInput = document.createElement('input');
   newInput.type = "checkbox";
   taskenter = document.querySelector('#entertext').value;
