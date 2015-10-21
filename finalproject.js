b58ad0e44c63fcb5282ab906f9535a6297889dd5
fawcbe01 <fawcbe01@luther.edu>
2015-10-20 19:29:30 -0500

    Update finalproject.js

--- finalproject.js
+++ finalproject.js
@@ -2,14 +2,25 @@ newTask = function() {
   myBox= document.querySelector("#textbox");
   myUl = document.querySelector("#tasklist");
   myLi = document.createElement("li");
-  myInput = document.createElement("Input");
+  myInput = document.createElement("INPUT");
   myInput.type="checkbox";
+  myText = document.createTextNode(myBox).value;
   myLi.appendChild(myInput);
-  myText = document.createTextNode(myBox.value);
   myLi.appendChild(myText);
   
   myPriority= document.querySelector("#priority");
   myLi.classList.add(myPriority.value);
 
 }
+
+ li = document.createElement('li');
+  newInput = document.createElement('input');
+  newInput.type = "checkbox";
+  newli.appendChild(newInput);
+  taskenter = document.querySelector('#entertext').value;
+  task = document.createTextNode(taskenter);
+  newli.appendChild(taskenter);
+  myUl.appendChild(newli);
+
+  
 
